dfa5e63891f6494c1d0c8b86389955d6b600fb69
Roger Colquehuanca <rogcolquehuancac@gmail.com>
2021-08-22 20:54:50 -0500

    #15 ProductCreate en Viewmodel AddProductViewModel.kt save state after adding a product and refresh ProductsFragment(List) previousBackStackEntry. 😔 difficult

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/addproduct/AddProductViewModel.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/addproduct/AddProductViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.douglasmotta.whitelabeltutorial.R
+import br.com.douglasmotta.whitelabeltutorial.domain.model.Product
 import br.com.douglasmotta.whitelabeltutorial.domain.usecase.ICreateProductUseCase
 import br.com.douglasmotta.whitelabeltutorial.util.fromCurrency
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -28,6 +29,9 @@ class AddProductViewModel @Inject constructor(
     private val _priceFieldErrorResId = MutableLiveData<Int>()
     val priceFieldErrorResId: LiveData<Int> = _priceFieldErrorResId
 
+    private val _productCreated = MutableLiveData<Product>()
+    val productCreated: LiveData<Product> = _productCreated
+
 
     private var isFormValid = false
     fun createProduct(description: String, price: String, imageUri: Uri?) = viewModelScope.launch {
@@ -40,6 +44,8 @@ class AddProductViewModel @Inject constructor(
         if (isFormValid) {
             try {
                 val product = createProductUsecase(description, price.fromCurrency(), imageUri!!)
+                _productCreated.value=product
+
             } catch (e: Exception) {
                 Log.d("CreateProduct: ", e.toString())
             }
